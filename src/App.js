f7cf67e562fb355ce246d1d2c0132f37665fb299
Thwin Khant Nyar Zaw <thwinkhantnyarzaw@gmail.com>
2025-04-16 11:12:53 +0000

    onw

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Footer from "./components/Footer";
-import Resume from "./components/Resume/ResumeNew";
 import {
   BrowserRouter as Router,
   Route,
-  Routes,
-  Navigate
+  Routes
 } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop";
 import "./style.css";
